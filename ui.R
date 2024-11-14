b7bca4093b5dff4b404f1c03d094aebf380bb9ce
stephen30ppp <122594459+stephen30ppp@users.noreply.github.com>
2024-11-14 23:44:32 +0800

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,49 @@
+# ui.R
+
+library(shiny)
+library(shinyWidgets)
+library(shinyBS)
+library(DT)
+
+ui <- fluidPage(
+  # Title panel sets text in the browser tab
+  div(
+    titlePanel(
+      title="", 
+      windowTitle=HTML(paste0("Mental Wellness Index™ Tool"))
+    ),
+    style="display:none"
+  ),
+  
+  navbarPage(
+    collapsible = TRUE,
+    title = if (show_mitre) {
+      div(
+        a(
+          href="https://www.mitre.org/",
+          img(src="media/MITRE_logo.png", height="30"),
+          target="blank"
+        ),
+        HTML(paste0("Mental Wellness Index™ Tool"))
+      )
+    } else {
+      div(
+        HTML(paste0("Mental Wellness Index™ Tool")),
+        "style" = "padding-top:5px"
+      )
+    },
+    theme = "stylesheets/app.css",
+    
+    # ... (其他标签页内容保持不变)
+    
+    # Copyright footer
+    if (show_mitre){
+      HTML(paste0(
+        "<span class = 'copyright-footer'>&copy; ",
+        format(Sys.Date(), "%Y"),
+        ", The MITRE Corporation",
+        "</span>"
+      ))
+    }
+  )
+)
