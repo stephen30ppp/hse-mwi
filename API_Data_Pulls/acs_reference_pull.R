c13dfc3fdd7ec9e3ab3de55e348361b4331f94d0
soda4fries <soda4fries@users.noreply.github.com>
2024-12-26 18:19:56 +0000

    style: format R code

--- API_Data_Pulls/acs_reference_pull.R
+++ API_Data_Pulls/acs_reference_pull.R
@@ -17,27 +17,33 @@ acs_data <- get_acs(
   variables = c(
     "B01001_001E", # Estimate!!Total: SEX BY AGE (population)
     "B01001B_001E" # SEX BY AGE (BLACK OR AFRICAN AMERICAN ALONE)
-  ), 
+  ),
   year = 2021,
   output = "wide"
 )
 
 # remove margin columns
 acs_data <- as.data.frame(acs_data)
-acs_data <- acs_data[, c(T,T,!grepl("M", colnames(acs_data)[-c(1:2)]))]
+acs_data <- acs_data[, c(T, T, !grepl("M", colnames(acs_data)[-c(1:2)]))]
 
 # rename columns
-acs_data <- rename(acs_data, 
-                   total_pop = B01001_001E,
-                   total_black = B01001B_001E)
+acs_data <- rename(acs_data,
+  total_pop = B01001_001E,
+  total_black = B01001B_001E
+)
 
 # write out data ----
 
 data_folder <- file.path(
-  gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
+  gsub("\\\\", "/", gsub("OneDrive - ", "", Sys.getenv("OneDrive"))),
   "Health and Social Equity - SJP - BHN Score Creation",
-  "Data", "Resources")
+  "Data", "Resources"
+)
 
-write.csv(acs_data, file = file.path(data_folder,
-                                      "ACS_ZCTA_Total_Populations.csv"), 
-          row.names = F, na = "")
+write.csv(acs_data,
+  file = file.path(
+    data_folder,
+    "ACS_ZCTA_Total_Populations.csv"
+  ),
+  row.names = F, na = ""
+)
