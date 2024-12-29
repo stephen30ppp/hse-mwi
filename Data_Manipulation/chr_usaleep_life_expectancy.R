d08897f793c0aa1e8f437df239a88d890e83a05e
stephen30ppp <122594459+stephen30ppp@users.noreply.github.com>
2024-12-29 19:38:14 +0800

    Revert "Revert "fix: Code Optimization and Refactoring: Modern Libraries, Redundant Variables, and Robust Error Handling""

--- Data_Manipulation/chr_usaleep_life_expectancy.R
+++ Data_Manipulation/chr_usaleep_life_expectancy.R
@@ -2,147 +2,86 @@
 # By Hannah De los Santos
 # Originated on: 10/13/21
 
-# supporting functions ----
+# Load required libraries
+library(tidyverse)
+library(usmap)
+library(readr)
+library(dplyr)
+library(stringr)
 
-# function to map county names to their fips codes
-# cty_df: dataframe with county and state columns
-# st_code: column name of state codes
-# cty_name: column name of county names
-# returns vector of mapped numbers
-cty_name_map <- function(cty_df, st_code_col, cty_name_col){
-  geoid_res <-
-    sapply(1:nrow(cty_df), function(x){
-      st_code <- tolower(cty_df[x, st_code_col])
-      cty_name <- tolower(gsub(
-        "Saint ", "St. ", 
-        gsub("St ", "St. ", 
-             cty_df[x, cty_name_col])
-      ))
-      
-      poss_fips <- county_fips$fips[grepl(st_code, 
-                                          tolower(county_fips$abbr)) &
-                                      grepl(cty_name,
-                                            tolower(county_fips$county))]
-      
-      # if multiple come up, we want the closest 
-      if (length(poss_fips) > 1){
-        cty_suffix <- if (st_code == "la"){
-          "parish"
-        } else {
-          "county"
-        }
-        
-        # don't add county suffix if it's a city
-        cty_check <- if (!grepl("city", cty_name)){
-          paste0(cty_name, " ", cty_suffix)
-        } else {
-          cty_name
-        }
-        
-        poss_fips <- poss_fips[which.min(
-          c(adist(cty_check,
-                  tolower(county_fips$county[county_fips$fips %in% poss_fips])))
-        )]
-      } else if (length(poss_fips) == 0){
-        # name is coded slightly wrong
-        wrong_name_map <- c(
-          "Prince Georges" = "Prince George's",
-          "De Kalb" = "DeKalb",
-          "La Porte" = "LaPorte",
-          "St. Marys" = "St. Mary's",
-          "Matanuska Susitna" = "Matanuska-Susitna",
-          "Prince Of Wales Hyder" = "Prince Of Wales-Hyder",
-          "Hoonah Angoon" = "Hoonah-Angoon",
-          "Obrien" = "O'Brien",
-          "De Soto" = "DeSoto",
-          "De Witt" = "DeWitt",
-          "La Salle" = "LaSalle"
-        )
-        names(wrong_name_map) <- tolower(names(wrong_name_map))
-        wrong_name_map <- tolower(wrong_name_map)
-        
-        
-        poss_fips <- county_fips$fips[grepl(st_code, 
-                                            tolower(county_fips$abbr)) &
-                                        grepl(wrong_name_map[cty_name],
-                                              tolower(county_fips$county))]
-      }
-      
-      return(poss_fips)
-    } )
-  
-  return(geoid_res)
+# Helper function to dynamically construct file paths
+construct_path <- function(base_folder, ...) {
+  file.path(gsub("\\\\", "/", gsub("OneDrive - ", "", Sys.getenv("OneDrive"))), base_folder, ...)
 }
 
-# load data and libraries ----
+# Function to map county names to FIPS codes
+map_county_to_fips <- function(cty_df, state_col, county_col, county_fips) {
+  cty_df %>%
+    mutate(
+      State_Lower = str_to_lower(!!sym(state_col)),
+      County_Lower = str_replace_all(str_to_lower(!!sym(county_col)), c("st " = "st. ", "saint " = "st. ")),
+      FIPS = map2_chr(State_Lower, County_Lower, function(state, county) {
+        fips_candidates <- county_fips %>%
+          filter(str_detect(str_to_lower(abbr), state) & str_detect(str_to_lower(county), county)) %>%
+          pull(fips)
+        
+        if (length(fips_candidates) > 1) {
+          suffix <- ifelse(state == "la", "parish", "county")
+          adjusted_county <- ifelse(str_detect(county, "city"), county, paste(county, suffix))
+          fips_candidates[which.min(adist(adjusted_county, str_to_lower(county_fips$county[fips_candidates])))]
+        } else if (length(fips_candidates) == 0) {
+          correction_map <- c(
+            "prince georges" = "prince george's", "de kalb" = "dekalb", 
+            "la porte" = "laporte", "st. marys" = "st. mary's",
+            "matanuska susitna" = "matanuska-susitna", "obrien" = "o'brien",
+            "de soto" = "desoto", "la salle" = "lasalle"
+          )
+          corrected_county <- correction_map[county] %||% county
+          county_fips %>%
+            filter(str_detect(str_to_lower(abbr), state) & str_detect(str_to_lower(county), corrected_county)) %>%
+            pull(fips)
+        } else {
+          fips_candidates
+        }
+      })
+    ) %>%
+    pull(FIPS)
+}
 
-library(usmap)
+# Load data
+resource_folder <- construct_path("Health and Social Equity - SJP - BHN Score Creation", "Data", "Resources")
+data_folder <- construct_path("Health and Social Equity - SJP - BHN Score Creation", "Data", "Raw", "Life_Expectancy")
 
-# supporting data -- resource folder
-resource_folder <-file.path(
-  gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
-  "Health and Social Equity - SJP - BHN Score Creation",
-  "Data", "Resources")
+county_fips <- read_csv(file.path(resource_folder, "county_name_to_fips.csv"), col_types = cols(fips = col_character()))
+cty_le <- read_csv(file.path(data_folder, "CHR_Life_Expectancy_2017-2019.csv"))
+ct_le <- read_csv(file.path(data_folder, "USALEEP_Life_Expectancy.CSV"), col_types = cols(
+  Tract.ID = col_character(), STATE2KX = col_character(), CNTY2KX = col_character(), TRACT2KX = col_character()
+))
 
-# supporting data -- data folder
-data_folder <- file.path(
-  gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
-  "Health and Social Equity - SJP - BHN Score Creation",
-  "Data", "Raw", "Life_Expectancy")
+# Data cleaning
+cty_le <- cty_le %>%
+  mutate(across(everything(), ~if_else(. == "100+", "100", .))) %>%
+  mutate(across(where(is.character), as.numeric))
+  # Map FIPS codes to counties
+  mutate(
+    ST_ABBR = if_else(State == "District of Columbia", "DC", state.abb[match(State, state.name)]),
+    CNTY_NUM = map_county_to_fips(cty_le, "ST_ABBR", "County", county_fips)
+  )
+  column_to_rownames("CNTY_NUM")
 
-# load county names to fips codes -- taken from usmap library
-county_fips <- read.csv(file.path(resource_folder, "county_name_to_fips.csv"),
-                        colClasses = c("fips" = "character"))
+# Update ct_le with additional life expectancy calculations
+ct_le <- ct_le %>%
+  mutate(
+    # Map CNTY_NUM to County values
+    CNTY_NUM = str_c(STATE2KX, CNTY2KX),
+    
+    baseline = cty_le[CNTY_NUM, "County.Value"],
+    black_le = cty_le[CNTY_NUM, "Black"],
+    modifier = black_le / baseline,
+    Life.Expectancy_black = if_else(is.na(estimate * modifier), estimate, estimate * modifier),
+    Life.Expectancy_pop = estimate
+  )
 
-
-# load life expectancy at county levels
-cty_le <- read.csv(file.path(data_folder, 
-                             "CHR_Life_Expectancy_2017-2019.csv"))
-# make all the 100+ to 100, convert all necessary columns to numeric
-cty_le[cty_le == "100+"] <- 100
-cty_le[, c(3,5:ncol(cty_le))] <- 
-  sapply(cty_le[, c(3,5:ncol(cty_le))],as.numeric)
-# map all the names to geoid numbers
-cty_le$ST_ABBR <- setNames(state.abb, state.name)[cty_le$State]
-cty_le$ST_ABBR[cty_le$State == "District of Columbia"] <- "DC"
-cty_le$CNTY_NUM <- cty_name_map(cty_le, "ST_ABBR", "County")
-
-# read in life expectancy at census tract level
-ct_le <- read.csv(file.path(data_folder, 
-                            "USALEEP_Life_Expectancy.CSV"),
-                  colClasses = c("Tract.ID" = "character",
-                                 "STATE2KX" = "character",
-                                 "CNTY2KX" = "character",
-                                 "TRACT2KX" = "character"
-                  ))
-ct_le$CNTY_NUM <- paste0(ct_le$STATE2KX, ct_le$CNTY2KX)
-
-# extrapolate life expectancy ----
-
-# make it easy to map county expectancy
-rownames(cty_le) <- cty_le$CNTY_NUM
-
-# extrapolate life expectancy using county as baseline
-ct_le$baseline <- cty_le[ct_le$CNTY_NUM, "County.Value"]
-ct_le$black_le <- cty_le[ct_le$CNTY_NUM, "Black"]
-ct_le$modifier <- ct_le$black_le/ct_le$baseline
-
-# create modified life expectancy
-ct_le$Life.Expectancy_black <- ct_le$estimate*ct_le$modifier
-ct_le$Life.Expectancy_pop <- ct_le$estimate
-
-# fill in where the county data is missing with baseline
-ct_le$Life.Expectancy_black[is.na(ct_le$Life.Expectancy_black)] <-
-  ct_le$estimate[is.na(ct_le$Life.Expectancy_black)]
-
-# write out ----
-
-data_folder <- file.path(
-  gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
-  "Health and Social Equity - SJP - BHN Score Creation",
-  "Data", "Preprocessed")
-
-write.csv(ct_le, 
-          file = file.path(data_folder,
-                           "CHR_USALEEP_Life_Expectancy.csv"), 
-          row.names = F, na = "")
+# Write results
+output_folder <- construct_path("Health and Social Equity - SJP - BHN Score Creation", "Data", "Preprocessed")
+write_csv(ct_le, file.path(output_folder, "CHR_USALEEP_Life_Expectancy.csv"), na = "")
